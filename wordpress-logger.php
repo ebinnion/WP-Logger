baa196d418f5a69af81ca4e2d480f58e7d00418e
ebinnion <ericbinnion@gmail.com>
2014-05-22 20:26:48 -0500

    Updates doc block for filter_search_query

--- wordpress-logger.php
+++ wordpress-logger.php
@@ -249,6 +249,8 @@ class WP_Logger {
 	 * because filter_logger_admin_search removes search paremter within query. Lack of the search
 	 * parameter in query causes the header to show `Search results for ""` with no search query.
 	 *
+	 * @global WP_Post $post Contains the current $post within the loop.
+	 *
 	 * @param mixed $search Contents of the search query variable.
 	 * @return mixed $search Udpated contents of the search query variable.
 	 */
