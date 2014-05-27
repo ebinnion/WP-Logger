d9c1501db806b26dccf13a96f00656b8995f7cf3
ebinnion <ericbinnion@gmail.com>
2014-05-27 00:24:06 -0500

    Adds method docs and comments to WP_Logger_API

--- class-wp-logger-api.php
+++ class-wp-logger-api.php
@@ -28,11 +28,23 @@ class WP_Logger_API {
 	 */
 	private $logs;
 
+	/**
+	 * Adds a callback for the init action hook.
+	 */
 	function __construct() {
 
 		add_action( 'init', array( $this, 'init' ), 2 );
 	}
 
+	/**
+	 * Checks if WP Logger plugin is installed, initializes $plugin_slug member data
+	 * and initializes array of logs in $logs member data.
+	 *
+	 * @see Function/method/class relied on
+	 * @link URL
+	 * @global WP_Post $post The global WP_Post object.
+	 *
+	 */
 	function init() {
 		global $post;
 
@@ -57,6 +69,7 @@ class WP_Logger_API {
 		if( self::$wp_logger_exists ) {
 			WP_Logger::register_plugin( $this->plugin_slug );
 
+			// Query to get all of the logs (posts) associated with this plugin
 			$logs = new WP_Query(
 				array(
 					'post_type' => 'wp-logger',
@@ -70,6 +83,7 @@ class WP_Logger_API {
 				)
 			);
 
+			// Save these logs in an array of post_name => WP_Post
 			if ( $logs->have_posts() ) {
 				while ( $logs->have_posts() ) {
 					$logs->the_post();
@@ -80,28 +94,46 @@ class WP_Logger_API {
 		}
 	}
 
+	/**
+	 * Saves the developers support email address in the options table.
+	 *
+	 * @param  string $email The email address.
+	 * @return bool True if email was saved/updated and false if email save/update failed.
+	 */
 	function register_plugin_email( $email ) {
 		$plugin_slug = preg_replace( '#(^[^\/]+).*#', '$1', plugin_basename( __FILE__ ) );
 		$key = $plugin_slug . '_email';
 		
-		update_option( $key, sanitize_email( $email ) );
+		return update_option( $key, sanitize_email( $email ) );
 	}
 
-	function build_slug( $string ) {
-		return 'wp-' . sanitize_title( $string );
-	}
-
+	/**
+	 * Acts as a wrapper to the add_entry method in WP_Logger.
+	 *
+	 * First, checks to see if WP Logger plugin is installed before going any further. Will
+	 * then check to see if there is an existing log matching the $log that the developer has called.
+	 * If there is an existing log with the same slug, then a WP_Post object is passed to WP_Logger:add_entry,
+	 * else, a the sluggified log name is passed to WP_Logger:add_entry().
+	 *
+	 * @param  string $message The message to log.
+	 * @param  string $log The log to add the message to.
+	 * @return WP_Error
+	 */
 	function add_entry( $message = '', $log = 'message' ) {
 		if ( ! self::$wp_logger_exists ) {
-			return false;
+			return new WP_Error( 'plugin-not-installed', esc_html__( 'The WP Logger plugin must be installed.', 'wp-logger-api' ) );
 		}
 
 		if ( empty( $message ) ) {
 			return new WP_Error( 'missing-parameter', esc_html__( 'You must pass a message as the first parameter.', 'wp-logger-api' ) );
 		}
 
+		// Call WP_Logger's prefix_slug method to sluggify post_name. Used to compare against $this->logs 
+		// and see if log already exists.
 		$prefixed_log = WP_Logger::prefix_slug( $log, $this->plugin_slug );
 
+		// If log already exists, then pass the WP_Post object. Else, pass the $log name and the log post
+		// will be created in WP_Logger:add_entry().
 		if( isset( $this->logs[ $prefixed_log ] ) ) {
 			return WP_Logger::add_entry( $this->logs[ $prefixed_log ], $message, $this->plugin_slug );
 		} else {
