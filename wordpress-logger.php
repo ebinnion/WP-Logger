029cae299e29c3fbfa8f3973ccd577eb8cfd8a9f
ebinnion <ericbinnion@gmail.com>
2014-05-27 21:41:10 -0500

    Init CPT and taxonomy as soon as possible.
    Using default priority caused issues errors due to taxonomy not being
    set yet.

--- wordpress-logger.php
+++ wordpress-logger.php
@@ -39,7 +39,7 @@ class WP_Logger {
 
 		self::$instance = $this;
 
-		add_action( 'init',             array( $this, 'init' ) );
+		add_action( 'init',             array( $this, 'init' ), 1 );
 		add_action( 'admin_menu',       array( $this,'add_menu_page' ) );
 		add_filter( 'comments_clauses', array( $this, 'add_comment_author' ), 10, 2 );
 	}
